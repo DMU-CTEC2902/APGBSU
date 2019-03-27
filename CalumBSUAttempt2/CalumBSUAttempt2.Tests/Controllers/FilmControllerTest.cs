50cd2320ab6fe8d41accce08e003626bfe21b304
p17164113 <p17164113@GWHXX653WIN016W>
2019-03-27 15:22:37 +0000

    Test Templates

---
+++ CalumBSUAttempt2/CalumBSUAttempt2.Tests/Controllers/FilmControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using CalumBSUAttempt2.Controllers;
+
+namespace CalumBSUAttempt2.Tests.Controllers
+{
+    [TestClass]
+    public class FilmControllerTest
+    {
+        [TestMethod]
+        public void Films()
+        {
+            //arrange
+            FilmsController controller = new FilmsController();
+
+            //act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ CalumBSUAttempt2/CalumBSUAttempt2.Tests/DirectorControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using CalumBSUAttempt2.Controllers;
+
+namespace CalumBSUAttempt2.Tests.Controllers
+{
+    [TestClass]
+    public class DirectorControllerTest
+    {
+        [TestMethod]
+        public void Director()
+        {
+            //arrange
+            DirectorsController controller = new DirectorsController();
+
+            //act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
