c6b10f1c1904c619dc8198e6093c2e59322e421e
Kevin Obeng <P1722971X@my365.dmu.ac.uk>
2019-03-27 18:38:43 +0000

    re done genre due to error load

---
+++ CalumBSUAttempt2/CalumBSUAttempt2.Tests/Controllers/GenreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using CalumBSUAttempt2.Controllers;
+
+namespace CalumBSUAttempt2.Tests.Controllers
+{
+    [TestClass]
+    public class GenreController
+    {
+        [TestMethod]
+        public void Genre()
+        {
+            //arrange
+            GenreController controller = new GenreController();
+
+            //act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
