6f16fa18f1c9d9250e2f27239194347a93239f0b
RhysCruz <rhyscruz16@gmail.com>
2019-03-27 22:01:51 +0000

    Test for Calum

--- CalumBSUAttempt2/CalumBSUAttempt2/Migrations/Configuration.cs
+++ CalumBSUAttempt2/CalumBSUAttempt2/Migrations/Configuration.cs
@@ -20,7 +20,7 @@ namespace CalumBSUAttempt2.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-            
+            /*
             Genre genre1 = new Genre();
             genre1.GenreId = 1;
             genre1.GenreName = "Comedy";
@@ -34,6 +34,32 @@ namespace CalumBSUAttempt2.Migrations
             genre3.GenreName = "Horror";
 
             base.Seed(context);
-    }
+            */
+
+            context.Actors.AddOrUpdate(actor => actor.ActorId,
+                new Actor() { ActorId = 1, ActorImage = "https://m.media-amazon.com/images/M/MV5BMjExOTY3NzExM15BMl5BanBnXkFtZTgwOTg1OTAzMTE@._V1_UX214_CR0,0,214,317_AL_.jpg", ActorName = "Michael B Jordan", ActorBio = "Antagonist in Black Panther", ActorDOB = new DateTime(09 / 03 / 1987), Rating = 8m, User = "rhyscruz" },
+                new Actor() { ActorId = 2, ActorImage = "https://m.media-amazon.com/images/M/MV5BMjA1MTQ3NzU1MV5BMl5BanBnXkFtZTgwMDE3Mjg0MzE@._V1_UY317_CR52,0,214,317_AL_.jpg", ActorName = "Liam Neeson", ActorBio = "Kosh wielding pub merchant", ActorDOB = new DateTime(07 / 06 / 1952), Rating = 7m, User = "calum ewart" }
+                );
+
+            context.Films.AddOrUpdate(films => films.FilmId,
+              new Film() { FilmId = 1, FilmImage = "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg", FilmName = "Black Panther", FilmDescription = "Marvel superhero movie based on the black panther comics", ReleaseDate = new DateTime(09 / 03 / 2018), Rating = 8m, User = "rhyscruz", FilmLength = 187, GenreId = 1 }
+
+                );
+
+            context.Directors.AddOrUpdate(director => director.DirectorId,
+              new Director() { DirectorId = 1, DirectorImage = "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg", DirectorName = "Black Panther", DirectorBio = "Marvel superhero movie based on the black panther comics", DirectorDOB = new DateTime(09 / 03 / 1978), Rating = 8m, User = "rhyscruz" }
+
+                );
+
+            context.Genres.AddOrUpdate(genre => genre.GenreId,
+              new Genre() { GenreId = 1, GenreName = "Comedy", User = "calum ewart" }
+
+              );
+
+            context.News.AddOrUpdate(news => news.NewsId,
+              new News() { NewsId = 1, Date = new DateTime(27/03/2019), Headline = "End Game Spoilers!", BodyText = "Ant Man Defeates Thanos by shrinkings and *****", User = "rhys cruz"}
+
+              );
+        }
     }
 }
